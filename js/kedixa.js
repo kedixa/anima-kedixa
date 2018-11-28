351cc6a39b70a23f05e0e63f99f1d6a674d63b4e
kedixa <1204837541@qq.com>
2018-11-28 16:16:37 +0800

    bottom corner style

--- js/kedixa.js
+++ js/kedixa.js
@@ -1,12 +1,20 @@
 jQuery(document).ready(function(){
 	jQuery("#show-qrcode").mouseover(function() {
 		jQuery("#qrcode-wrapper").show();
+		jQuery("#show-qrcode").css("background-color", "#D0422C");
 	});
 	jQuery("#show-qrcode").mouseout(function() {
 		jQuery("#qrcode-wrapper").hide();
+		jQuery("#show-qrcode").css("background-color", "rgba(230,230,230,0.5)");
+	});
+	jQuery("#to-top").mouseover(function() {
+		jQuery("#to-top").css("background-color", "#D0422C");
+	});
+	jQuery("#to-top").mouseout(function() {
+		jQuery("#to-top").css("background-color", "rgba(230,230,230,0.5)");
 	});
 	jQuery("#to-top").on("click", function(){
-		jQuery("html").animate({ scrollTop: 0 }, 200);
+		jQuery("html").animate({ scrollTop: 0 }, 500);
 	});
 });
 
